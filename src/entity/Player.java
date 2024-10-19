0e3db70741f5fa134e8b8448a2fd4636e61ecca0
HUST-8xbet <dongchayleminhfifa@gmail.com>
2024-10-19 23:55:44 +0700

    update

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -86,7 +86,7 @@ public class Player extends Entity{
     {
         drawX = camera.worldToScreenX(worldX);
         drawY = camera.worldToScreenY(worldY);
-        System.out.println(drawX + " " + drawY);
+        camera.update((int)worldX, (int)worldY, currentMap.getMapWidth(), currentMap.getMapHeight());
         g2.drawImage(player_sprite[currentAnimationState][currentFrames] ,
                 (int)drawX , (int)drawY , 48*2, 48*2, null);
     }
