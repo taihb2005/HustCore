ca3f8e58f7bcca09f325b7843650b93172f90aa1
NguyenQuy <nguyenducquy10072005@gmail.com>
2024-11-28 16:54:00 +0700

    map

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -59,7 +59,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     private void loadMap()
     {
-        MapParser.loadMap( "map_test" ,"res/map/map3.tmx");
+        MapParser.loadMap( "map_test" ,"res/map/map1.tmx");
         currentMap = MapManager.getGameMap("map_test");
         camera.setCamera(windowWidth , windowHeight , currentMap.getMapWidth() ,currentMap.getMapHeight());
         pFinder = new PathFinder(currentMap);
